e122c9e94780d08ccf73e5be3a0586086abb3257
Xuuxxi <2933171792@qq.com>
2022-03-24 13:19:28 +0800

    Initial commit

---
+++ src/test/java/com/example/ego/EgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
