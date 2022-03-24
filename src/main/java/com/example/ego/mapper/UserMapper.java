2679a05f4c0ad811e114f76f67339e5945e48831
Xuuxxi <2933171792@qq.com>
2022-03-24 15:37:02 +0800

    test

--- src/main/java/com/example/ego/mapper/UserMapper.java
+++ src/main/java/com/example/ego/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.example.ego.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.ego.entity.User;
 
-public interface UserMapper extends BaseMapper {
+public interface UserMapper extends BaseMapper<User> {
 }
